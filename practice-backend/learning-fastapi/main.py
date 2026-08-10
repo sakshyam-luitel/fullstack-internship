7524d096afed2e397f0512d37bd19ccee58c9e6d
Sakshyam Luitel <sakshyam73@gmail.com>
2026-08-10 21:02:48 +0545

    Made some fastapi endpoints using restapi for learning purposes

--- practice-backend/learning-fastapi/main.py
+++ practice-backend/learning-fastapi/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from fastapi.params import Body
+from typing import Optional
+from random import randrange
+from fastapi import Response , status , HTTPException
 app = FastAPI()
+
+my_posts = [
+    {"title" : "title of post 1", "content" : "content of post 1" , "id" : 1},
+    {"title" : "favourite foods" , "content" : "I like pizza" , "id" : 2},
+]
 
 @app.get('/')
 async def root():
-    return {'hello':'world'}
+    return {'hello':'my friend'}
 
 @app.get('/items/{item_id}')
 async def read_item(item_id : int):
@@ -34,7 +42,7 @@ fake_items_db = [
 ]
 
 
-@app.get('/items')
+@app.get('/item')
 async def read_item(skip : int = 0 , limit : int = 10):
     return fake_items_db[skip : skip + limit]
 
@@ -47,3 +55,75 @@ class Item(BaseModel):
 @app.post("/items")
 async def create_item(item : Item):
     return item
+
+@app.get("/posts")
+def get_posts():
+    return { "data" : my_posts}
+
+class Post(BaseModel):
+    title : str
+    content : str
+    published : bool = True
+    rating : Optional[int] = None
+
+@app.post("/posts" , status_code = status.HTTP_201_CREATED)
+def create_posts(post : Post):
+    print(post)
+    print(post.dict())
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0 , 1000000000)
+    my_posts.append(post_dict)
+    return {"data" : post_dict}\
+
+def find_post(id):
+    for post in my_posts:
+        if post['id'] == id:
+            return post
+
+@app.get('/posts/latest')
+def get_post():
+    post = my_posts[-1]
+    return {"details" : post}
+
+
+@app.get('/posts/{id}')
+def get_post(id : int , response : Response):
+    post = find_post(id)
+
+    if not post : 
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND , detail = f"post with id : {id} was not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message" : f"post with id: {id} was not found"}
+    return {"post_detail" : post}
+
+@app.delete('/posts/{id}' , status_code = status.HTTP_204_NO_CONTENT)
+def delete_posts(id : int , post : Post):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail= f"post with id : {id} was not found"
+        )
+    my_posts.remove(post)
+    return Response(status_code = status.HTTP_204_NO_CONTENT)
+
+def find_index_post(id):
+    for index , post in enumerate(my_posts):
+        if post['id'] == id:
+            return index
+        
+@app.put('/posts/{id}' ,status_code = status.HTTP_200_OK )
+def update_post(id : int , post : Post):
+    index = find_index_post(id)
+    if not post:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail= f"post with id : {id} was not found"
+            )
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {'data' : post_dict}
+
+    
+
